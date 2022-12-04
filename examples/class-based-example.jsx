b85930aa39cc4508d9d242a1805071d1af260ef9
JiDew1571 <ji.dew1571@gmail.com>
2022-12-03 22:22:54 -0800

    first commit

---
+++ examples/class-based-example.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Example extends Component {
+  render() {
+    return <div className="div">Hi</div>;
+  }
+}
+
+// dont worry, class based components are not used any more.
---
+++ examples/functional-componets.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example = () => {
+  return <div className="div">Hello, This is a functional component.</div>;
+};
+
+export default Example;
+
+// you can see how easy it is.
